f8d6967517fbe44b73bde79c66ac35400b792f97
wakako <wakako.otsuka@rakus-partners.co.jp>
2019-08-06 10:07:07 +0900

    first commit

---
+++ src/main/java/com/example/demo/ExInetrmediateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExInetrmediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExInetrmediateApplication.class, args);
+	}
+
+}
